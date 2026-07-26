b606d635844d1693d983d89be8f8d460861817de
alexrilodev <alex4rl96@gmail.com>
2026-07-26 20:37:38 +0200

    Fix navigation android device

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { App } from '@capacitor/app';
 import { AuthService } from './services/auth.service';
 import { NotificationService } from './services/notification.service';
 import { ThemeService } from './services/theme.service';
@@ -14,7 +17,9 @@ export class AppComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private notificationService: NotificationService,
-    private themeService: ThemeService
+    private themeService: ThemeService,
+    private location: Location,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -24,6 +29,18 @@ export class AppComponent implements OnInit {
       filter(user => !!user)
     ).subscribe(() => {
       this.notificationService.initialize();
+    });
+
+    this.setupBackButton();
+  }
+
+  private setupBackButton() {
+    App.addListener('backButton', ({ canGoBack }) => {
+      if (canGoBack) {
+        this.location.back();
+      } else {
+        this.router.navigateByUrl('/tabs/mapa');
+      }
     });
   }
 }
